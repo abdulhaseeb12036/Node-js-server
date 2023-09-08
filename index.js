e0e57f8083f51f3f183a5a9adf9de4b17333ebe7
Abdul Haseeb <abdulhaseeb120336@gmail.com>
2023-09-08 23:34:42 +0500

    Listener for home added

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ const DataModel=require('./Models/Data_collection')
 const app=express()
 app.use(express.json())
 
-const connection=mongoose.connect("mongodb+srv://abdulhaseeb:haseeb810@haseeb.r7vjfx7.mongodb.net/?retryWrites=true&w=majority")
+async  function getConnected(){
+    const connection=mongoose.connect("mongodb+srv://abdulhaseeb:haseeb810@haseeb.r7vjfx7.mongodb.net/?retryWrites=true&w=majority")
 
-connection.then((conn)=>{
-    console.log("Connection Succesful")
-})
-connection.catch((error)=>{
-    console.log("Connection Error "+ error)
-})
+
+
+
+    connection.then((conn)=>{
+        console.log("Connection Succesful")
+        main()
+    })
+
+    connection.catch((error)=>{
+        console.log("Connection Error "+ error)
+    })
+}
+
+
+getConnected()
+
+
+async function main(){
+    app.listen(process.env.Port || 10000)
+ console.log("Listning is Started")
+
+   app.get('/',(req,res)=>{
+    console.log(`Request Received at home`)
+    res.send(`<h1>Hello ${req.ip} i received your request </h1>`)
+   })
+}
